870eb2e38ddc3660a4bccfe48ad8c986b6fdf692
Djack1010 <giachi.iada@gmail.com>
2018-07-15 14:37:43 +0200

    Upload script to balance buggy/non-buggy instances in the DB

---
+++ GeneralScriptsFolder/balance.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+if [ -f "$SCRIPTPATH/base_COM" ] || [ -f "$SCRIPTPATH/base_SeT" ]; then
+    echo "ERROR, base_COM or base_SeT not found"
+    exit
+fi
+
+function progrBar {
+    #[##################################################] (100%)
+    echo -e "\033[3A"
+    PAR=$1
+    TOT=$2
+    PER=$(bc <<< "scale = 2; ($PAR / $TOT) * 100" | cut -d'.' -f1)
+    TEMPPER=$( echo $PER | cut -d'.' -f1)
+    COUNT=0
+    echo "PROGRESS: $PAR out of $TOT"
+    echo -ne "["
+    while [ "$TEMPPER" -gt "0" ]; do
+        TEMPPER=$(($TEMPPER-2))
+        echo -ne "#"
+        COUNT=$(($COUNT+1))
+    done
+    COUNT=$((50-$COUNT))
+    for (( c=0; c<$COUNT; c++ )); do
+        echo -ne "-"
+    done  
+    echo -ne "] ($PER%)"
+    if [ ! -z "$PIDRUN" ]; then
+        TIMERUN=$( ps -o etime= -p "$PIDRUN" )
+        echo -ne " TIME:$TIMERUN"
+    fi
+    echo ""
+}
+
+MUT=$(($(ls $SCRIPTPATH/base_COM | wc -l)-$(ls $SCRIPTPATH/base_COM | grep "_0" | wc -l)))
+MUT2=$(($(ls $SCRIPTPATH/base_SeT | wc -l)-$(ls $SCRIPTPATH/base_SeT | grep "_0" | wc -l)))
+if [ "$MUT" != "$MUT2" ]; then
+    echo "ERROR! base_COM and base_SeT files do not match in number"
+    exit
+fi
+ORI=$(($(ls $SCRIPTPATH/base_COM | wc -l)-$MUT))
+echo "TOT: $(ls $SCRIPTPATH/base_COM | wc -l) MUT: $MUT ORI: $ORI"
+DIFF=$(($MUT-$ORI))
+IND=0
+echo -e "DIFF: $DIFF\n\n"
+while [ "$IND" != "$DIFF" ]; do
+    PASS=0
+    while [ "$PASS" == "0" ]; do
+        FILETOMOVE=$(ls $SCRIPTPATH/generic/base_COM | shuf -n 1)
+        if [ "$(find $SCRIPTPATH/base_COM -name "$FILETOMOVE")" ]; then
+            continue
+        else
+            PASS=1
+        fi
+    done
+    cp $SCRIPTPATH/generic/base_COM/$FILETOMOVE $SCRIPTPATH/base_COM
+    cp $SCRIPTPATH/generic/base_SeT/$FILETOMOVE $SCRIPTPATH/base_SeT
+    progrBar $IND $DIFF
+    IND=$(($IND+1))
+done
